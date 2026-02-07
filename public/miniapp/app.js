d7e325f6edf40b0da5dc693866d281730396c60f
Povarhbv2 <ponpon56@mail.ru>
2026-02-07 23:05:48 +0300

    app.js

--- public/miniapp/app.js
+++ public/miniapp/app.js
@@ -1,7 +1,12 @@
-fetch("/data.json")
+const tg = window.Telegram?.WebApp;
+if (tg) tg.expand();
+
+// ВАЖНО: ../ потому что data.json лежит в public
+fetch("../data.json?v=1")
   .then(res => res.json())
   .then(data => {
     const catalog = document.getElementById("catalog");
+    catalog.innerHTML = "";
 
     data.products.forEach(p => {
       const div = document.createElement("div");
@@ -10,11 +15,20 @@ fetch("/data.json")
         <h3>${p.name}</h3>
         <p>${p.price}</p>
         <p>В наличии: ${p.stock}</p>
+        <button onclick='order(${JSON.stringify(p)})'>
+          Заказать
+        </button>
       `;
 
       catalog.appendChild(div);
     });
   })
-  .catch(err => console.log(err));
+  .catch(err => console.log("Ошибка:", err));
 
-
+function order(product) {
+  if (tg) {
+    tg.sendData(JSON.stringify(product));
+  } else {
+    alert("Тестовый заказ");
+  }
+}
